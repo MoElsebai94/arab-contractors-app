b615b233b999b76e6ea599fc43a068c1965bd5e5
MoElsebai94 <mohamedelsebai@hotmail.co.uk>
2026-01-13 12:13:12 +0100

    Fix API_BASE for production environment

--- client/src/pages/Dalots.jsx
+++ client/src/pages/Dalots.jsx
@@ -6,7 +6,9 @@ import {
 import { useLanguage } from '../context/LanguageContext';
 import './Dalots.css';
 
-const API_BASE = 'http://localhost:3001/api';
+const API_BASE = window.location.hostname === 'localhost'
+    ? 'http://localhost:3001/api'
+    : '/api';
 
 // Helper to get auth headers
 const getAuthHeaders = () => {
